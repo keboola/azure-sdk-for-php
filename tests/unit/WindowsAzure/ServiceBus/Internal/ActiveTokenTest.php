0f97355efa31d857b15a369e6929fb970634f227
zajca <zajca@zajca.cz>
2023-02-15 15:42:08 +0100

    fix namespaces

--- tests/unit/WindowsAzure/ServiceBus/Internal/ActiveTokenTest.php
+++ tests/unit/WindowsAzure/ServiceBus/Internal/ActiveTokenTest.php
@@ -23,7 +23,7 @@
  * @link      https://github.com/WindowsAzure/azure-sdk-for-php
  */
 
-namespace Tests\Unit\WindowsAzure\ServiceBus\Models;
+namespace Tests\Unit\WindowsAzure\ServiceBus\Internal;
 
 use WindowsAzure\ServiceBus\Internal\ActiveToken;
 use WindowsAzure\ServiceBus\Internal\WrapAccessTokenResult;
